20feaaf7d2cb824448f86bc5c3aa1da4e79d4eba
us_vm_c <wsh@gsvm1.com>
2022-02-14 18:18:36 +0800

    fixbug

---
+++ config/data_structure.py
@@ -0,0 +1,28 @@
+
+from ff_utils.data_accessing.db import table_name_dict
+
+s_indicator_dict = {
+    'is_2stick': 'smallint',
+    'is_mm_tt_1': 'smallint',
+    'high_1year_days': 'smallint',
+    'high_1year_change':'real',
+    'sctr':'real',
+    'sctr_score':'real',
+}
+
+i_indicator_dict = {
+    'trend': 'smallint',
+    'sctr':'real',
+    'sctr_score':'real',
+}
+
+
+
+
+
+
+
+
+
+
+
--- tools/util_tools.py
+++ tools/util_tools.py
@@ -1,3 +1,16 @@
+
+from ff_utils.tools import util_tools as ut
+from ff_utils.data_accessing.db import PGEngine, table_name_dict
+
+# region datetime
+def get_ak_td_array(end_date=ut.get_today_str()):
+    td_df=PGEngine.table2df(table_name_dict['dc_ak_trade_date'])
+    td_array = td_df['trade_date']
+    return td_array[td_array<=end_date]
+
+# endregion datetime
+
+
 # region dpp_s
 def is_ak_daily_updated():
     
