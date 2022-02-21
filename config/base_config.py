21c49880ce1a85d690ea9a2894c6406aaf3d8e6d
us_vm_c <wsh@gsvm1.com>
2022-02-21 14:08:04 +0800

    f

---
+++ config/base_config.py
@@ -0,0 +1,10 @@
+
+sctr_params = {
+    'l_ema_param': 200,              'l_ema_ratio': 0.3,
+    'l_roc_param': 125,              'l_roc_ratio': 0.3,
+    'm_ema_param': 50,               'm_ema_ratio': 0.15,
+    'm_roc_param': 20,               'm_roc_ratio': 0.15,
+    's_ppo_param': (12, 26, 3),      's_ppo_ratio': 0.05,
+    's_rsi_param': 14,               's_rsi_ratio': 0.05,
+    'round_n': 2
+}
