e96b88908a160e6bd7c061f39522ac127f39edd0
us_vm_c <wsh@gsvm1.com>
2022-02-21 14:01:09 +0800

    f

--- config/data_structure.py
+++ config/data_structure.py
@@ -1,13 +1,27 @@
 
 from ff_utils.data_accessing.db import table_name_dict
 
-s_indicator_dict = {
+s_row_indicator_dict = {
     'is_2stick': 'smallint',
     'is_mm_tt_1': 'smallint',
     'high_1year_days': 'smallint',
     'high_1year_change':'real',
+    'high_2year_days': 'smallint',
+    'high_2year_change':'real',
     'sctr':'real',
+    'ma10':'real',
+    'ma20':'real',
+    'ma50':'real',
+    'ma200':'real',
+}
+
+s_col_indicator_dict = {
     'sctr_score':'real',
+}
+
+i_indicator_dict = {
+    'trend': 'smallint',
+    'sctr':'real',
     'ma10':'real',
     'ma20':'real',
     'ma50':'real',
@@ -15,13 +29,7 @@ s_indicator_dict = {
 }
 
 i_indicator_dict = {
-    'trend': 'smallint',
-    'sctr':'real',
     'sctr_score':'real',
-    'ma10':'real',
-    'ma20':'real',
-    'ma50':'real',
-    'ma200':'real',
 }
 
 cache_name_dict = {
--- tools/util_tools.py
+++ tools/util_tools.py
@@ -1,6 +1,14 @@
 
+
+import talib as ta
+import pandas as pd
+import orjson
+
+
 from ff_utils.tools import util_tools as ut
 from ff_utils.data_accessing.db import PGEngine, table_name_dict
+from si_utils.config import base_config as con
+from si_utils.tools import cache
 
 # region datetime
 def get_ak_td_array(end_date=ut.get_today_str()):
@@ -12,11 +20,36 @@ def get_ak_td_array(end_date=ut.get_today_str()):
 
 
 # region dpp_s
-def is_ak_daily_updated():
-    
-    pass
+def get_cached_ak_s_daily():
+    ak_daily_keys = [x for x in cache.dc_cache.keys() if x.startswith('dc_ak@') and x.endswith('@daily')]
+    ak_daily_values = list(map(lambda x:orjson.loads(orjson.loads(cache.dc_cache_get_value(x))['data_value']), ak_daily_keys))
+    ak_daily_df = pd.DataFrame(ak_daily_values)
+    return ak_daily_df
 
 
 # endregion dpp_s
 
+# region indicators
+def sctr(input_array, sctr_params=con.sctr_params):
+    def _n(_array):
+        return (_array-min(_array))/(max(_array)-min(_array))
+    l_ema = _n(ta.EMA(input_array, timeperiod=sctr_params['l_ema_param']).dropna())
+    l_roc = _n(ta.ROC(input_array, timeperiod=sctr_params['l_roc_param']).dropna())
+    m_ema = _n(ta.EMA(input_array, timeperiod=sctr_params['m_ema_param']).dropna())
+    m_roc = _n(ta.ROC(input_array, timeperiod=sctr_params['m_roc_param']).dropna())
+    s_ppo = ta.PPO(input_array, fastperiod=sctr_params['s_ppo_param'][0], slowperiod=sctr_params['s_ppo_param'][1]).dropna()
+    s_ppo_h = _n((s_ppo - ta.EMA(s_ppo, timeperiod=sctr_params['s_ppo_param'][2])).dropna())
+    s_rsi = _n(ta.RSI(input_array, timeperiod=sctr_params['s_rsi_param']).dropna())
+    sctr_array = l_ema * sctr_params['l_ema_ratio'] + l_roc * sctr_params['l_roc_ratio'] + \
+                m_ema * sctr_params['m_ema_ratio'] + m_roc * sctr_params['m_roc_ratio'] + \
+                s_ppo_h * sctr_params['s_ppo_ratio'] + s_rsi * sctr_params['s_rsi_ratio']
+    sctr_array = sctr_array.dropna()
+    return sctr_array
 
+def ma_n(input_array, n_list=[10, 20, 50, 200]):
+    ma_list = []
+    for n in n_list:
+        ma_list.append(ta.EMA(input_array, timeperiod=n))
+    return pd.concat(ma_list, columns=list(map(lambda x:'ma{0}'.format(x), n_list)))
+
+# endregion indicators
